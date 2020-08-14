48b7ffd831e44cdbd8a776f841ed0d2bdc9c61ba
Venki <venkateshprasad728@gmail.com>
2020-08-15 01:48:51 +0530

    Updates

---
+++ precache-manifest.2917670ea5e3939e827368c4d7110e68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c668688938ccc91f8e665fd981f3f06c",
+    "url": "/MyPortfolio-React/index.html"
+  },
+  {
+    "revision": "fb925e7ed2c5a644373d",
+    "url": "/MyPortfolio-React/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4913c52ce90ff765eb6c",
+    "url": "/MyPortfolio-React/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "fb925e7ed2c5a644373d",
+    "url": "/MyPortfolio-React/static/js/2.82fd4a8d.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/MyPortfolio-React/static/js/2.82fd4a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4913c52ce90ff765eb6c",
+    "url": "/MyPortfolio-React/static/js/main.b369ab7c.chunk.js"
+  },
+  {
+    "revision": "a35ca8338bc50bbcee1a",
+    "url": "/MyPortfolio-React/static/js/runtime-main.8cc13c77.js"
+  },
+  {
+    "revision": "0423b6b3221094498ffc28b6c7129709",
+    "url": "/MyPortfolio-React/static/media/Avatar.0423b6b3.jpeg"
+  }
+]);
